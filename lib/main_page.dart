04f42854fbf77d7043cc8dd8dd81b5befb457dd8
EvGen94 <sharanov_evgenii@mail.ru>
2020-08-04 11:06:17 +0300

    ratio 1.7

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -12,41 +12,34 @@ class _MainPageState extends State<MainPage> {
   List<Widget> listWidget = List<Widget>();
   List<List<Widget>> listOfLists = List<List<Widget>>();
 
-  var _pageContraller = PageController();
-
-  final _controller = PageController(viewportFraction: 1);
 
 
-  
-  final _controllerRow = PageController(viewportFraction: 1);
+   var _controllerRow = PageController();
 
-  void nextPage() async {
-    await _pageContraller.nextPage(
-      duration: Duration(milliseconds: 1750),
-      curve: Curves.easeInOutQuad,
-    );
-  }
-
-  void prevPage() async {
-    await _pageContraller.previousPage(
-      duration: Duration(milliseconds: 1750),
-      curve: Curves.easeInOutQuad,
-    );
-  }
+  var _controllerColumn = PageController();
 
   List<Widget> _getNeumorphicButton(int index) {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 8; i++) {
       listWidget.add(_buildNeumorphicButton(i + 1));
     }
     listOfLists.add(listWidget);
     listWidget = List<Widget>();
 
     return listOfLists[index];
-    // return listWidget;
   }
 
   @override
   Widget build(BuildContext context) {
+   
+    _controllerRow = PageController(
+      viewportFraction: 0.353 * (MediaQuery.of(context).size.height/MediaQuery.of(context).size.width) + 0.254,);
+     // viewportFraction: 1);
+    
+     _controllerColumn = PageController(
+      viewportFraction: -0.128 * (MediaQuery.of(context).size.height/MediaQuery.of(context).size.width) + 1.1,);
+      //viewportFraction: 0.865);
+    
+
     return Scaffold(
       backgroundColor: Colors.blueGrey.shade200,
       body: PageView(
@@ -86,7 +79,7 @@ class _MainPageState extends State<MainPage> {
           ),
         ],
         scrollDirection: Axis.vertical,
-        controller: _controller,
+        controller: _controllerColumn,
       ),
     );
   }
@@ -94,44 +87,24 @@ class _MainPageState extends State<MainPage> {
   Widget _buildNeumorphicButton(int amount) {
     return Padding(
       padding: EdgeInsets.only(
-          left: 0.25 *
-              MediaQuery.of(context).size.width *
-              (((MediaQuery.of(context).size.height /
-                          MediaQuery.of(context).size.width) *
-                      0.3) +
-                  0.2),
-          top: 0.25 *
-              MediaQuery.of(context).size.width *
-              (((MediaQuery.of(context).size.height /
-                          MediaQuery.of(context).size.width) *
-                      0.3) +
-                  0.2)),
+          left: MediaQuery.of(context).size.width * 0.001,
+          top: MediaQuery.of(context).size.width * 0.001),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           // SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-          /* GestureDetector(
-              onPanUpdate: (details) {
-                if (details.delta.dx < 0) {
-                  nextPage();
-                }
-                if (details.delta.dx > 0) {
-                  prevPage();
-                }
-              },*/
-          //  child:
           Container(
-            height: 1.5 *
-                MediaQuery.of(context).size.width *
-                (((MediaQuery.of(context).size.height /
-                            MediaQuery.of(context).size.width) *
-                        0.3) +
-                    0.2),
-            width: MediaQuery.of(context).size.width *
-                (((MediaQuery.of(context).size.height /
-                            MediaQuery.of(context).size.width) *
-                        0.3) +
-                    0.2),
+            height: (MediaQuery.of(context).size.height /
+                        MediaQuery.of(context).size.width <
+                    1.5)
+                ? MediaQuery.of(context).size.height * 0.94
+                : (MediaQuery.of(context).size.width * 0.94) * 1.7,
+            width: (MediaQuery.of(context).size.height /
+                        MediaQuery.of(context).size.width >
+                    1.5)
+                ? MediaQuery.of(context).size.width * 0.94
+                : (MediaQuery.of(context).size.height * 0.94) / 1.7,
             child: Card(
               semanticContainer: true,
               clipBehavior: Clip.antiAliasWithSaveLayer,
@@ -169,7 +142,7 @@ class _MainPageState extends State<MainPage> {
                         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper augue ac massa egestas, non luctus purus cursus',
                         style: TextStyle(fontSize: 22),
                         minFontSize: 20,
-                        maxLines: 3,
+                        maxLines: 2,
                         overflow: TextOverflow.ellipsis,
                       ),
                     ),
@@ -178,7 +151,6 @@ class _MainPageState extends State<MainPage> {
               ),
             ),
           ),
-          //  ),
         ],
       ),
     );
