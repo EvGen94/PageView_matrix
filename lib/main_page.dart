7811952ea63f5b2ba5f86e9500bb625440091402
EvGen94 <sharanov_evgenii@mail.ru>
2020-08-06 23:08:56 +0300

    Zoom_demo

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'dart:math';
+
+const SCALE_FRACTION = 0.7;
+const FULL_SCALE = 1.0;
+const PAGER_HEIGHT = 420.0;
 
 class MainPage extends StatefulWidget {
   const MainPage({Key key}) : super(key: key);
@@ -12,15 +17,13 @@ class _MainPageState extends State<MainPage> {
   List<Widget> listWidget = List<Widget>();
   List<List<Widget>> listOfLists = List<List<Widget>>();
 
-
-
-   var _controllerRow = PageController();
+  var _controllerRow = PageController();
 
   var _controllerColumn = PageController();
 
   List<Widget> _getNeumorphicButton(int index) {
     for (int i = 0; i < 8; i++) {
-      listWidget.add(_buildNeumorphicButton(i + 1));
+      listWidget.add(_buildNeumorphicButton(3));
     }
     listOfLists.add(listWidget);
     listWidget = List<Widget>();
@@ -28,17 +31,34 @@ class _MainPageState extends State<MainPage> {
     return listOfLists[index];
   }
 
+  double page = 2;
+  double viewPortFraction = 0.5;
+  int currentPage = 2;
+  PageController pageControllerZoom;
+  @override
+  void initState() {
+    pageControllerZoom = PageController(
+        initialPage: currentPage, viewportFraction: viewPortFraction);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-   
     _controllerRow = PageController(
-      viewportFraction: 0.353 * (MediaQuery.of(context).size.height/MediaQuery.of(context).size.width) + 0.254,);
-     // viewportFraction: 1);
-    
-     _controllerColumn = PageController(
-      viewportFraction: -0.128 * (MediaQuery.of(context).size.height/MediaQuery.of(context).size.width) + 1.1,);
-      //viewportFraction: 0.865);
-    
+      viewportFraction: 0.353 *
+              (MediaQuery.of(context).size.height /
+                  MediaQuery.of(context).size.width) +
+          0.254,
+    );
+    // viewportFraction: 1);
+
+    _controllerColumn = PageController(
+      viewportFraction: -0.128 *
+              (MediaQuery.of(context).size.height /
+                  MediaQuery.of(context).size.width) +
+          1.1,
+    );
+    // viewportFraction: 0.45);
 
     return Scaffold(
       backgroundColor: Colors.blueGrey.shade200,
@@ -47,10 +67,31 @@ class _MainPageState extends State<MainPage> {
           Row(
             children: <Widget>[
               Expanded(
-                child: PageView(
-                  children: _getNeumorphicButton(0),
-                  //  physics: NeverScrollableScrollPhysics(),
-                  controller: _controllerRow,
+                child: NotificationListener<ScrollNotification>(
+                  onNotification: (ScrollNotification notification) {
+                    if (notification is ScrollUpdateNotification) {
+                      setState(() {
+                        page = pageControllerZoom.page;
+                      });
+                    }
+                  },
+                  child: PageView.builder(
+                    onPageChanged: (pos) {
+                      setState(() {
+                        currentPage = pos;
+                      });
+                    },
+                    physics: BouncingScrollPhysics(),
+                    controller: pageControllerZoom,
+                    itemCount: _getNeumorphicButton(0).length,
+                    itemBuilder: (context, index) {
+                      final scale = max(
+                          SCALE_FRACTION,
+                          (FULL_SCALE - (index - page).abs()) +
+                              viewPortFraction);
+                      return _buildNeumorphicButton(scale);
+                    },
+                  ),
                 ),
               ),
             ],
@@ -84,27 +125,30 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  Widget _buildNeumorphicButton(int amount) {
+  Widget _buildNeumorphicButton(double scale) {
     return Padding(
       padding: EdgeInsets.only(
           left: MediaQuery.of(context).size.width * 0.001,
-          top: MediaQuery.of(context).size.width * 0.001),
+          top: 300),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           // SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+
           Container(
-            height: (MediaQuery.of(context).size.height /
-                        MediaQuery.of(context).size.width <
-                    1.5)
-                ? MediaQuery.of(context).size.height * 0.94
-                : (MediaQuery.of(context).size.width * 0.94) * 1.7,
-            width: (MediaQuery.of(context).size.height /
-                        MediaQuery.of(context).size.width >
-                    1.5)
-                ? MediaQuery.of(context).size.width * 0.94
-                : (MediaQuery.of(context).size.height * 0.94) / 1.7,
+            /* height: (MediaQuery.of(context).size.height /
+                          MediaQuery.of(context).size.width <
+                      1.5)
+                  ? MediaQuery.of(context).size.height * 0.94
+                  : (MediaQuery.of(context).size.width * 0.94) * 1.7,
+              width: (MediaQuery.of(context).size.height /
+                          MediaQuery.of(context).size.width >
+                      1.5)
+                  ? MediaQuery.of(context).size.width * 0.94
+                  : (MediaQuery.of(context).size.height * 0.94) / 1.7, */
+            height: PAGER_HEIGHT * scale,
+            width: PAGER_HEIGHT * scale,
             child: Card(
               semanticContainer: true,
               clipBehavior: Clip.antiAliasWithSaveLayer,
@@ -113,42 +157,7 @@ class _MainPageState extends State<MainPage> {
               ),
               color: Colors.grey[50],
               elevation: 10,
-              child: Column(
-                children: <Widget>[
-                  Stack(
-                    children: <Widget>[
-                      Image.asset('assets/third.jpg', fit: BoxFit.fill),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: 45.0, right: 15.0, top: 15.0),
-                        child: Center(
-                          child: AutoSizeText(
-                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.grey[50],
-                                  fontFamily: 'Roboto2'),
-                              minFontSize: 10,
-                              maxLines: 6,
-                              overflow: TextOverflow.ellipsis),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(18.0),
-                      child: AutoSizeText(
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper augue ac massa egestas, non luctus purus cursus',
-                        style: TextStyle(fontSize: 22),
-                        minFontSize: 20,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+           
             ),
           ),
         ],
